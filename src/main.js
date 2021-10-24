e128fb0a29a983fbebf9f83a27683f7781a55857
gcxfd <i@rmw.link>
2021-10-24 09:29:55 +0800

    🔶

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from 'vue';
+import main from './main.vue';
 
-createApp(App).mount(document.body)
+createApp(main).mount(document.body);

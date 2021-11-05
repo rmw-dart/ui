ed26ba80c12e5ffe1d00a2ffa375669ab139c7e8
gcxfd <i@rmw.link>
2021-11-05 10:35:09 +0800

    🔶

--- src/home.vue
+++ src/home.vue
@@ -24,7 +24,7 @@ setup: =>
 </script>
 
 <template lang="pug">
-main(style="overflow-y: scroll; height: 100%")
+div(style="overflow-y: scroll; height: 100%")
   nav(
     style="display: flex; padding: 14px; font-weight: 300; line-height: 14px; font-size: 14px; color: #aaa; position: fixed; top: 0; border-bottom: 1rpx solid #aaa; left: 0; right: 0; background-color: #fff"
   )
@@ -45,19 +45,22 @@ main(style="overflow-y: scroll; height: 100%")
       //-   :style="{ fontFamily: 'I', marginLeft: '14px' }",
       //-   v-html="'&#x' + i"
       //- )
-  section(style="font-size: 14px; font-weight: 300; padding: 14px")
-    header
-      a(style="margin-right: 2px") 月光博客
-      | ：
-    article
-      p(style="font-weight: 300; padding-top: 14px")
-        | 房产税如果普遍征收而没有任何豁免，由于二线以下城市房租无法拟补持有成本，那么房产投资客应该会抛掉二线以下的城市房产，集中购买一线城市的房产，那么二线以下城市的房地产市场估计会崩盘。
-      p(style="font-weight: 300; padding-top: 14px")
-        | 我觉得，如果每年让业主交几万元房产税，还想继续当业主的爸爸，把业主当儿子管，业主肯定会不干的。要想大量征收房产税，又想让业主闭嘴当孙子，这种可能性较低。
-      p 我觉得，如果每年让业主交几万元房产税，还想继续当业主的爸爸，把业主当儿子管，业主肯定会不干的。要想大量征收房产税，又想让业主闭嘴当孙子，这种可能性较低。
-      p 我觉得，如果每年让业主交几万元房产税，还想继续当业主的爸爸，把业主当儿子管，业主肯定会不干的。要想大量征收房产税，又想让业主闭嘴当孙子，这种可能性较低。
-      p 我觉得，如果每年让业主交几万元房产税，还想继续当业主的爸爸，把业主当儿子管，业主肯定会不干的。要想大量征收房产税，又想让业主闭嘴当孙子，这种可能性较低。
-      p 我觉得，如果每年让业主交几万元房产税，还想继续当业主的爸爸，把业主当儿子管，业主肯定会不干的。要想大量征收房产税，又想让业主闭嘴当孙子，这种可能性较低。
+  main(
+    style="font-size: 14px; font-weight: 300; padding: 14px; top: 42px; position: absolute; bottom: 0"
+  )
+    section
+      header
+        a(style="margin-right: 2px") 月光博客1
+        | ：
+      article
+        p(style="padding-top: 14px")
+          | 房产税如果普遍征收而没有任何豁免，由于二线以下城市房租无法拟补持有成本，那么房产投资客应该会抛掉二线以下的城市房产，集中购买一线城市的房产，那么二线以下城市的房地产市场估计会崩盘。
+        p(style="padding-top: 14px")
+          | 房产税如果普遍征收而没有任何豁免，由于二线以下城市房租无法拟补持有成本，那么房产投资客应该会抛掉二线以下的城市房产，集中购买一线城市的房产，那么二线以下城市的房地产市场估计会崩盘。
+        p(style="padding-top: 14px")
+          | 房产税如果普遍征收而没有任何豁免，由于二线以下城市房租无法拟补持有成本，那么房产投资客应该会抛掉二线以下的城市房产，集中购买一线城市的房产，那么二线以下城市的房地产市场估计会崩盘。
+        p(style="padding-top: 14px")
+          | 房产税如果普遍征收而没有任何豁免，由于二线以下城市房租无法拟补持有成本，那么房产投资客应该会抛掉二线以下的城市房产，集中购买一线城市的房产，那么二线以下城市的房地产市场估计会崩盘。
 </template>
 
 <style>
